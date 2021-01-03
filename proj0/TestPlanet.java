b761a3831a81a32a7899e58b44b5210aa5c84dd1
wxy <wangxizhi@zju.edu.cn>
2021-01-03 19:07:14 +0800

    remove

--- proj0/TestPlanet.java
+++
@@ -1,7 +0,0 @@
-public class TestPlanet {
-	public static void main(String[] args) {
-		Planet a = new Planet(1.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
-		Planet b = new Planet(2.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");	
-		System.out.print(a.calcForceExertedBy(b));
-	}
-}
