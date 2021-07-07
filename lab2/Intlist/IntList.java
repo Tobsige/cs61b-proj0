b2cf4086835ac82d0e2b022dacad39f2913353f5
wxy <wangxizhi@zju.edu.cn>
2021-07-07 10:56:27 +0800

    finish

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList C = A;
+        while(C.rest != null){
+            C = C.rest;
+        }
+        C.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +96,16 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList C = new IntList(A.first, null);
+        IntList L = C;
+        IntList La = A.rest;
+        while (La != null){
+            L.rest = new IntList(La.first, null);
+            L = L.rest;
+            La = La.rest;
+        }
+        dcatenate(C, B);
+        return C;
     }
 
 
