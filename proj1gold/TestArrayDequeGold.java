865be3bade0b838d46e1794608d03e1970f0b90f
wxy <wangxizhi@zju.edu.cn>
2021-07-25 21:01:42 +0800

    proj1 3 -- 4 failure messages

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -2,11 +2,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDequeGold {
-    private void compareDeques(StudentArrayDeque stu, ArrayDequeSolution slo) {
+    private void compareDeques(StudentArrayDeque stu, ArrayDequeSolution slo, String mthd) {
         for (int i = 0; i < slo.size(); i++) {
             int x = (int) stu.removeFirst(), y = (int) slo.removeFirst();
 
-            String msg = "There are problems in addFirst(), student was " + x +", correct was " + y;
+            String msg = "There are problems in " + mthd + ", student was " + x +", correct was " + y;
             assertEquals(msg, y, x);
         }
     }
@@ -40,15 +40,72 @@ public class TestArrayDequeGold {
     }
 
     @Test
-    public void autoG(){
+    public void addT() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> hp = new ArrayDequeSolution<>();
+
+        addItem(sad1, hp, 100);
+        removeItem(sad1, hp, 99);
+
 
         for (int i = 0; i < 20; i++) {
             sad1.addFirst(i);
             hp.addFirst(i);
         }
 
-        compareDeques(sad1, hp);
+        String mthd = "addFirst";
+        compareDeques(sad1, hp, mthd);
+
     }
+
+    @Test
+    public void addLT() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> hp = new ArrayDequeSolution<>();
+
+        addItem(sad1, hp, 100);
+        removeItem(sad1, hp, 100);
+
+        for (int i = 0; i < 20; i++) {
+            sad1.addLast(i);
+            hp.addLast(i);
+        }
+        String mthd1 = "addLast";
+        compareDeques(sad1, hp, mthd1);
+    }
+
+    @Test
+    public void rmvT() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> hp = new ArrayDequeSolution<>();
+
+        //addItem(sad1, hp, 100);
+        //removeItem(sad1, hp, 100);
+
+        addItem(sad1, hp, 100);
+        for (int i = 0; i < 90; i++) {
+            sad1.removeFirst();
+            hp.removeFirst();
+        }
+        String mthd1 = "removeFirst";
+        compareDeques(sad1, hp, mthd1);
+    }
+
+    @Test
+    public void rmvLT() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> hp = new ArrayDequeSolution<>();
+
+        //addItem(sad1, hp, 100);
+        //removeItem(sad1, hp, 100);
+
+        addItem(sad1, hp, 100);
+        for (int i = 0; i < 90; i++) {
+            sad1.removeLast();
+            hp.removeLast();
+        }
+        String mthd1 = "removeLast";
+        compareDeques(sad1, hp, mthd1);
+    }
+
 }
